21f45f5a2aa78ad0f6088c888d28dcc4c3ea538c
Rafael <78313332+rafalo08@users.noreply.github.com>
2025-02-24 16:02:24 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -6,25 +6,3 @@
 	
 #if __name__ == '__main__':
 #	main()
-def main():
-    m, n = map(int, input().split())
-    H = list(map(int, input().split()))
-    D = list(map(int, input().split()))
-    
-    total_D = sum(D)
-    INF = float('inf')
-    dp = [INF] * (n + 1)
-    dp[0] = 0  # Caso base: salud 0 sin usar alimentos
-    
-    for h, d in zip(H, D):
-        # Iterar hacia atrás para evitar procesar el mismo alimento múltiples veces
-        for j in range(n, -1, -1):
-            if dp[j] != INF:
-                new_j = min(j + h, n)
-                if dp[new_j] > dp[j] + d:
-                    dp[new_j] = dp[j] + d
-    
-    print(total_D - dp[n])
-
-if __name__ == '__main__':
-    main()
